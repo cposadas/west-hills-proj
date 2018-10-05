0588c53e777fd2fce6c6dd8d6a1fba8de8274ff6
Carlos Posadas <CarlosPosadas@whccd.edu>
2018-10-05 14:50:56 -0700

    test file w/ production css reference

---
+++ web/bootstrap-detail-prod.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html class="no-js" lang="EN">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title>West Hills College</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" type="text/css" href="/_resources/css/src/app.css">
+    </head>
+    <body>
+        <div class="site js-site" id="site">
+            <?php require_once './partials/header.php' ?>
+            <main class="site__main-content js-main-content">
+                <?php
+                    require_once './partials/bootstrap-page-detail.php';
+                ?>
+            </main>
+            <?php require_once './partials/footer.php' ?>
+        </div>
+        <script type="text/javascript" src="assets/js/dist/app.min.js"></script>
+    </body>
+</html>
